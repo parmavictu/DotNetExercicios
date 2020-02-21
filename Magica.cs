2bc4fdec64e3a8ee391593b7b4e47125ba24ff81
Victor Gabriel Fernandes Parma <victorfp@ciandt.com>
2020-02-21 14:18:56 -0300

    first commit

---
+++ Magica.cs
@@ -0,0 +1,8 @@
+public class Magica
+{
+    public int somar(int a, int b)
+    {
+
+        return a+b;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Magica mgc = new Magica();
+            
+            Console.Write("Valor 1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Valor 1: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(mgc.somar(a,b));
+
+            Console.WriteLine("teste");
+        }
+    }
+
+
+    
+}
